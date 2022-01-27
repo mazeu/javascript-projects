27e4e2aa2baa1a2815ded9d91c7eb223838ccf34
mazeu <marciomazeu@hotmail.com>
2022-01-27 08:43:25 -0300

    memory status created

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const http = require('http');
 const host = 'http://localhost';
 const port = 3000;
 
+const stats = require('./pcRamUsage.js');
+
 console.log(http);
 
 http.createServer((req, res)=>{
-    res.write('<h1>working</h1>')
+    let url = req.url;
+
+    if(url === '/stats'){
+        res.end(JSON.stringify(stats, null, 2));
+    }else{
+        res.end('<h1>Bem vindo</h1>')
+    }
+    
 }).listen(port, ()=>console.log(`server is running in ${host}:${port}`));
