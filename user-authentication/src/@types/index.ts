ad6b9c0b6e8a2e381139aff4565fd13e8ccaeffd
mazeu <marciomazeu@hotmail.com.br>
2022-01-27 20:40:29 -0300

    Add typescript

--- user-authentication/src/@types/index.ts
+++ user-authentication/src/@types/index.ts
@@ -1,1 +1,12 @@
-console.log('Executei');
+import express,{Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+
+app.get('/status',(req: Request, res:Response, next:NextFunction)=>{
+    res.status(200).send({foo:'bar'});
+} );
+
+app.listen(3000, ()=> {
+    console.log('Aplicacao executando na porta 3000!')
+})
